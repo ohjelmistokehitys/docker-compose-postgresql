a8eebe3af8a09ba025f38dd1904f94661b8251ed
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2024-09-18 10:47:54 +0000

    Fixed variables -> volumes

--- .github/classroom/ex2-volumes.py
+++ .github/classroom/ex2-volumes.py
@@ -11,7 +11,7 @@ check("name" in result and "services" in result, "The compose file must be valid
 check("postgres" in result, "postgres service must be defined in the compose file")
 check("pgadmin" in result, "pgadmin service must be defined in the compose file")
 
-check("volumes" in result, "You must define required environment variables for both of the services")
+check("volumes" in result, "You must define required volumes for both of the services")
 
 check("/var/lib/postgresql" in result, "Use /var/lib/postgresql/data as the volume for the postgres service")
 check("/docker-entrypoint-initdb.d" in result, "Use /docker-entrypoint-initdb.d/ as the volume for the postgres service")
