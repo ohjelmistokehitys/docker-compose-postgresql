bb414f4c58f314707fc4ce248b48e4a9097158af
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2024-09-26 07:39:15 +0300

    Removed check for POSTGRES_USER env variable. It is an optional variable.

--- .github/classroom/ex1-environment.py
+++ .github/classroom/ex1-environment.py
@@ -13,7 +13,6 @@ check("pgadmin" in result, "pgadmin service must be defined in the compose file"
 
 check("environment" in result, "You must define required environment variables for both of the services")
 
-check("POSTGRES_USER" in result, "POSTGRES_USER environment variable is missing")
 check("POSTGRES_PASSWORD" in result, "POSTGRES_PASSWORD environment variable is missing")
 check("PGADMIN_DEFAULT_EMAIL" in result, "PGADMIN_DEFAULT_EMAIL environment variable is missing")
 check("PGADMIN_DEFAULT_PASSWORD" in result, "PGADMIN_DEFAULT_PASSWORD environment variable is missing")
